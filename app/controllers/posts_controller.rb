0f9dcc7cf3a5457272bd5f87cef2243538c0b2b3
Joseph Burke <joseph_burke@outlook.com>
2020-09-10 09:18:19 +0100

    Implement validations for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index
+    @posts = Post.all
   end
   
 
@@ -10,8 +11,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.build(params[:user])
-    redirect_to action:'index' if @post.save
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to action:'index'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
